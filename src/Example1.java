360afb0c0247ec4d7b76fc29859046a2ef584fe1
niknemo2009 <nezdoliy.yura@gmail.com>
2022-01-04 19:47:22 +0200

    commit 5

--- src/Example1.java
+++ src/Example1.java
@@ -1,6 +1,7 @@
 public class Example1 {
 
     public static void main(String[] args) {
+        int qwe=678;
         System.out.println("limited");
          System.out.println("test");
         if(true){
