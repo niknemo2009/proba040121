9ad96db08188f8289b06421e4b25a71051b10a1c
IlyaBurda <92583669+IlyaBurda@users.noreply.github.com>
2022-01-04 19:45:12 +0200

    add if operator with println

--- src/Example1.java
+++ src/Example1.java
@@ -1,8 +1,10 @@
 public class Example1 {
 
     public static void main(String[] args) {
-        int qwe=67;
         System.out.println("limited");
          System.out.println("test");
+        if(true){
+             System.out.println("Qualification");
+        }
     }
 }
