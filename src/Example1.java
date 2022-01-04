a5857c945ea2ff2ed596252af98bd6715f3e626f
IlyaBurda <92583669+IlyaBurda@users.noreply.github.com>
2022-01-04 19:36:38 +0200

    change prints

--- src/Example1.java
+++ src/Example1.java
@@ -1,6 +1,7 @@
 public class Example1 {
 
     public static void main(String[] args) {
-        System.out.println("dsfdf");
+        System.out.println("limited");
+         System.out.println("test");
     }
 }
