5d65af71df097a605024ae7a8d52fd5c72a43f11
niknemo2009 <nezdoliy.yura@gmail.com>
2022-01-04 19:57:37 +0200

    Update Example1.java

--- src/Example1.java
+++ src/Example1.java
@@ -1,6 +1,7 @@
 public class Example1 {
 
     public static void main(String[] args) {
+        int qaz=6789;
         System.out.println("limited");
          System.out.println("test");
          System.out.println("is cool");
