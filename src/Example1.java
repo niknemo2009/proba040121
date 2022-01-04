0f185d97d8a0df36ef895023d1352aa500a586cc
IlyaBurda <92583669+IlyaBurda@users.noreply.github.com>
2022-01-04 19:59:49 +0200

    add "super" print

--- src/Example1.java
+++ src/Example1.java
@@ -3,5 +3,6 @@ public class Example1 {
     public static void main(String[] args) {
         System.out.println("limited");
          System.out.println("test");
+         System.out.println("super");
     }
 }
